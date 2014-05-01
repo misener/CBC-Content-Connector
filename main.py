49969e74aa70ae50811e1b150da289c5814239a3
Dan <misener@misenermini.media.in.cbcsrc.ca>
2014-05-01 15:00:24 -0400

    Visual refresh
    Reorganized ID presentation on main page, changed default number of IDs
    to 5, updated to new Bootswatch and Bootstrap.

--- main.py
+++ main.py
@@ -14,17 +14,22 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         program = self.request.get('program', default_value="Spark")
-    	number_of_results = self.request.get('results', default_value="10")
+    	number_of_results = self.request.get('results', default_value="5")
 
         params = urllib.urlencode(
             { 'form': 'json',
               'range': '1-%s' % number_of_results,
-              'sort': 'pubDate|desc',
+              'sort': 'added|desc',
+              'pretty': 'true',
               'byCustomValue': '{show}{%s}' % program })
 
         url = 'http://feed.theplatform.com/f/h9dtGB/r3VD0FujBumK?%s' % params
 
+        logging.info(url)
+
         r = json.loads(urlfetch.fetch(url, deadline=20).content)
+
+        # logging.info(r)
 
         audioclips = []
 
@@ -37,12 +42,43 @@ class MainHandler(webapp2.RequestHandler):
             mediaIDurl = entry['media$content'][0]['plfile$releases'][0]['plrelease$mediaId']
             mediaID = re.search(r'\d{10}', mediaIDurl).group()
 
-            audioclips.append((audioclipID, mediaID, title))
+            metrics = entry['plmedia$metrics']
+
+            # logging.info(metrics)
+
+            audioclips.append((audioclipID, mediaID, title, metrics))
+
+
+        # Grab standalone player clips (with YoSpace IDs)
+        # For radio shows, this is pretty much always the only release
+        # turning it off November 1, 2013
+
+
+        # standaloneplayerclips = []
+
+        # for entry in r['entries']:
+        #     title = entry['title']
+        #     metrics = entry['plmedia$metrics']
+
+        #     for content in entry['media$content']:
+
+        #         for release in content['plfile$releases']:
+        #             audioclipIDurl = release['id']
+        #             audioclipID = re.search(r'\d{10}', audioclipIDurl).group()
+
+        #             mediaIDurl = release['plrelease$mediaId']
+        #             mediaID = re.search(r'\d{10}', mediaIDurl).group()
+
+        #             if 'pl1$yospaceID' in release:
+        #                 standaloneplayerclips.append((audioclipID, mediaID, title, metrics))
+
 
         template_values = {
             'audioclips': audioclips,
             'program': program,
-            'results': number_of_results }
+            'results': number_of_results,
+            # 'standaloneplayerclips': standaloneplayerclips
+            }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
